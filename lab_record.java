e465eda9ca65099a8168c8abb78ee7928e6321ab
Monish395 <143940618+Monish395@users.noreply.github.com>
2024-07-24 13:19:57 +0530

    Update

---
+++ lab_record.java
@@ -0,0 +1,15 @@
+
+
+
+//operator -> +
+
+public class Main{
+public static void main(String[] args){
+int s1 = 100+20;
+int s2 = s1+50;
+int s3 = s1+s2;
+System.out.println("s1 : "+s1);
+System.out.println("s2 : "+s2);
+System.out.println("s3 : "+s3);
+}
+}
